7453b0f7aab5097f2933af085af348b14cc98157
Kinhs <91020533+Kinhs@users.noreply.github.com>
2024-03-13 13:16:04 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/kinh/music_player/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kinh.music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Player',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Playlist'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    LogConfig logConfig = LogConfig(logType: LogType.DEBUG);
+    _audioQuery.setLogConfig(logConfig);
+
+    checkAndRequestPermissions();
+  }
+
+  checkAndRequestPermissions({bool retry = false}) async {
+    _hasPermission = await _audioQuery.checkAndRequest(
+      retryRequest: retry,
+    );
+    _hasPermission ? setState(() {}) : null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: !_hasPermission
+            ? noAccessToLibraryWidget()
+            : FutureBuilder<List<SongModel>>(
+                future: _audioQuery.querySongs(
+                  sortType: null,
+                  orderType: OrderType.ASC_OR_SMALLER,
+                  uriType: UriType.EXTERNAL,
+                  ignoreCase: true,
+                ),
+                builder: (context, item) {
+                  if (item.hasError) {
+                    return Text(item.error.toString());
+                  }
+                  if (item.data == null) {
+                    return const CircularProgressIndicator();
+                  }
+
+                  if (item.data!.isEmpty) return const Text("Nothing found!");
+
+                  return ListView.builder(
+                    itemCount: item.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(item.data![index].title),
+                        subtitle: Text(item.data![index].artist ?? "Unknown"),
+                        trailing: const Icon(Icons.arrow_forward_rounded),
+                        leading: const Icon(Icons.music_note),
+                      );
+                    },
+                  );
+                }
+              )
+      ),
+    );
+  }
+  Widget noAccessToLibraryWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.redAccent.withOpacity(0.5),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("Application doesn't have access to the library"),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => checkAndRequestPermissions(retry: true),
+            child: const Text("Allow"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
